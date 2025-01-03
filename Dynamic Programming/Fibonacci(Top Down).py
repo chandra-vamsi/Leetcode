6ec4457ebb08fd96854c534776e2b70e59dd1bd2
Chandra Vamsi Telugu <67367077+chandra-vamsi@users.noreply.github.com>
2025-01-03 13:14:34 +0530

    Create Fibonacci(Top Down).py

---
+++ Dynamic Programming/Fibonacci(Top Down).py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        memo={0:0,1:1}              #We Define Base cases here
+        def f(x):                   #Defining a function to recursively add elements into our memo
+            if x in memo:           #Check for the element in memo Hashset(This eliminates the recurrsive calculation)
+                return memo[x]      #if found Return
+            else:
+                memo[x]=f(x-1)+f(x-2) #if not now will add the branches(Top to down) 
+                return memo[x]      
+        return f(n)               #calling Function
