f3943111a0a23193a80c2c1286c75dc001ea4875
Chandra Vamsi Telugu <67367077+chandra-vamsi@users.noreply.github.com>
2025-01-06 08:45:48 +0530

    Update 70. Climbing Stairs.py

--- Dynamic Programming/70. Climbing Stairs.py
+++ Dynamic Programming/70. Climbing Stairs.py
@@ -1,5 +1,16 @@
 #70. Climbing Stairs.py
 #https://leetcode.com/problems/climbing-stairs/description/
+#Bottomup approach(Constant space) only used when we need previous two variables
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n==1:                          #Setting Base cases
+            return 1
+        if n==2:
+            return 2
+        prev,curr=1,2
+        for i in range(2,n):
+            prev,curr=curr,prev+curr     #similar to Fibonacci Series will set previous element to current and current element to sum of current+previous
+        return curr
 #Bottomup approach(Tabulation)
 class Solution:
     def climbStairs(self, n: int) -> int:
